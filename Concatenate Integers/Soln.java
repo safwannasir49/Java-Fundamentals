67304dd062eb4598b56a5c1a0362e4b6d47f9f29
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2026-02-04 17:24:00 +0530

    Create Soln.java

---
+++ Concatenate Integers/Soln.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Write Your Code to Concatenate and print'
+        //String result = Integer.toString(a) + Integer.toString(b);
+        System.out.println(""+a+b);
+        
+    }
+}
