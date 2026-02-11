55ed6bcc18294cad5333c97decba9704d56d796b
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2026-02-11 15:42:00 +0530

    Create Soln.java

---
+++ The Else Statements/Soln.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void utility(int number) {
+        
+        if (number > 100) {
+            System.out.println("Big");
+        } else {
+            System.out.println("Number");
+        }
+        
+    }
+}
