3e1dfb124a74b1358199e2bc3ef8a562705956e8
Safwan Nasir <127377148+safwannasir49@users.noreply.github.com>
2026-02-20 11:47:34 +0530

    Create Soln.java

---
+++ Print Without Newline/Soln.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void utility(Scanner sc) {
+        // code here
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        
+        System.out.print(s1+ " "+s2);
+    }
+}
